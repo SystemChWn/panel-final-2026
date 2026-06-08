bf3569b3afc563afa81d0f39fd810cc3240b6914
SystemChWn <sistemaschgw@gmail.com>
2026-06-08 10:12:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -301,7 +301,7 @@ st.dataframe(
     df_estilizado,
     use_container_width=True,
     hide_index=True,
-    height=800
+    height=460
 )
 
 # =========================================================
