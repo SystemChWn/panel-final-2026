26a4b1b7ba9e18d278307e08898b39cfa4374805
SystemChWn <sistemaschgw@gmail.com>
2026-06-19 10:18:57 -0600

    Update main.py

--- main.py
+++ main.py
@@ -97,7 +97,7 @@ def asignar_rondines_por_puntos(df):
             # Solo si detectamos un inicio (1-5) Y venimos de un final (>=40)
             punto_actual = pd.to_numeric(str(fila["Punto_QR"]).replace("Punto ", ""), errors="coerce")
             
-            if (punto_actual in [1, 2, 3, 4, 5, 6]) and (ultimo_pt >= 44):
+            if (punto_actual in [ 2, 3, 4, 5, 6]) and (ultimo_pt >= 44):
                 contador += 1
                 if contador > 6: contador = 0
         
